60a6c6fa66ae2b9aeeab1c3baa5de3d96f742330
ayahayman <ayahayman@live.com>
2025-07-22 02:50:14 +0300

    Add basic task manager.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val task1:Task = Task("Study", true)
+    val task2:Task = Task("Exercise", true)
+    val task3:Task = Task("Clean the house", false)
+    val taskList = mutableListOf<Task>(task1, task2, task3)
+
+    for (task in taskList){
+        if (task.isDone == true){
+            println("✅ ${task.title}")
+        }
+        else
+        {
+            println("❌ ${task.title}")
+        }
+    }
+}
+
+data class Task(val title:String, val isDone: Boolean) {
+}
